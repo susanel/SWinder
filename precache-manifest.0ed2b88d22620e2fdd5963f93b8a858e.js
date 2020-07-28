6e59cf89621dcc7a1615e3687379d7b6f3bcd6a9
Susanel92 <zuzannanizka@gmail.com>
2020-07-28 23:33:32 +0200

    Updates

---
+++ precache-manifest.0ed2b88d22620e2fdd5963f93b8a858e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77de0d4fde1382109240c4162de102e6",
+    "url": "/SWinder/index.html"
+  },
+  {
+    "revision": "a38f67e385e946dd67ca",
+    "url": "/SWinder/static/css/main.a7b5404e.chunk.css"
+  },
+  {
+    "revision": "e68f6110c0ac16f1840b",
+    "url": "/SWinder/static/js/2.859a9ec0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SWinder/static/js/2.859a9ec0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a38f67e385e946dd67ca",
+    "url": "/SWinder/static/js/main.d9fa6b69.chunk.js"
+  },
+  {
+    "revision": "bb4ad3240daf4eb07d9f",
+    "url": "/SWinder/static/js/runtime-main.c57617b5.js"
+  },
+  {
+    "revision": "4e2dd027b8f198abe175eca68cffa900",
+    "url": "/SWinder/static/media/space-1280x720.4e2dd027.png"
+  }
+]);
