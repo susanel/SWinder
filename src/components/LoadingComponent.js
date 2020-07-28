c0dab4d4f50d80318d22179338d3ef5fc11e7793
Susanel92 <zuzannanizka@gmail.com>
2020-07-28 17:00:44 +0200

    change <LoadingComponent/> animation
    Change:
    * from: spinner (FontAwesomes)
    * to: Kylo Ren <img /> and pulsing animation

--- src/components/LoadingComponent.js
+++ src/components/LoadingComponent.js
@@ -1,18 +1,12 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 
 const LoadingComponent = () => {
-  const style = { color: "#F1C40F" };
   return (
     <div className="loading">
-      <FontAwesomeIcon
-        className="spinner"
-        icon={faSpinner}
-        pulse
-        style={style}
-        size="6x"
-      />
+      <div className="image-container">
+        <img src="/assets/images/characters/kylo.jpg" alt="Kylo Ren" />
+      </div>
+      <p>Looking for potential matches in your Universe</p>
     </div>
   );
 };
