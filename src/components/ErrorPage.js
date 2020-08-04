bd59b45d359da73d0314f907cfa0cd34dc637232
Susanel92 <zuzannanizka@gmail.com>
2020-08-04 20:42:49 +0200

    add & style <ErrorPage>

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -1,7 +1,19 @@
 import React from "react";
+import yoda from "../assets/images/characters/19.jpg";
 
 const ErrorPage = () => {
-  return <p style={{ fontSize: 30, color: "yellow" }}>Nie ma takiej strony</p>;
+  return (
+    <div className="error">
+      <p className="big">Ocurred, a 404 error has...</p>
+      <div className="image-container">
+        <img src={yoda} alt="Yoda" />
+      </div>
+      <p>
+        The dark side I sense in you. Due to disturbance in the force I couldn't
+        find the page you were looking for.
+      </p>
+    </div>
+  );
 };
 
 export default ErrorPage;
